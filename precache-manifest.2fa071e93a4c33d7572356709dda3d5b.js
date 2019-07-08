efb15659588286819d57a3755f35a24a0168d02d
Ashley Evans <ashley_e@live.com>
2019-07-08 15:55:35 -0700

    Updates

---
+++ precache-manifest.2fa071e93a4c33d7572356709dda3d5b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8990b600fcd03e72ebf83c4e37d8cf75",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a27bbfda7f8f17542742",
+    "url": "/clicky-game/static/css/main.6f398caa.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "a27bbfda7f8f17542742",
+    "url": "/clicky-game/static/js/main.547a4f70.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "9127bb545acaeb9fb013bc30464d1acd",
+    "url": "/clicky-game/static/media/puppy1.9127bb54.jpg"
+  },
+  {
+    "revision": "2c4adc86015e84a9ebe448f46b78b959",
+    "url": "/clicky-game/static/media/puppy10.2c4adc86.jpg"
+  },
+  {
+    "revision": "c6922dbc48ab2b315e297d5dfa5aebaa",
+    "url": "/clicky-game/static/media/puppy11.c6922dbc.jpg"
+  },
+  {
+    "revision": "b6ba7359b80470e13f7a039b480aefdb",
+    "url": "/clicky-game/static/media/puppy12.b6ba7359.jpg"
+  },
+  {
+    "revision": "c9b2ff46f9ab375cd7a344c800c71bdf",
+    "url": "/clicky-game/static/media/puppy2.c9b2ff46.jpg"
+  },
+  {
+    "revision": "e9459fa3da28da4a6e5c24d137f5e329",
+    "url": "/clicky-game/static/media/puppy3.e9459fa3.jpg"
+  },
+  {
+    "revision": "159db36521eda29dd4b1d96313967958",
+    "url": "/clicky-game/static/media/puppy4.159db365.jpg"
+  },
+  {
+    "revision": "c37756a8ab6d7beff18b10ef5587e5ef",
+    "url": "/clicky-game/static/media/puppy5.c37756a8.jpg"
+  },
+  {
+    "revision": "beaa43a609388f347cae61245ad1a280",
+    "url": "/clicky-game/static/media/puppy6.beaa43a6.jpg"
+  },
+  {
+    "revision": "22719ef667b01d1dd61a6795a47d2af6",
+    "url": "/clicky-game/static/media/puppy7.22719ef6.jpg"
+  },
+  {
+    "revision": "48ad79437047a2003ebbcbf589fda554",
+    "url": "/clicky-game/static/media/puppy8.48ad7943.jpg"
+  },
+  {
+    "revision": "96aab14511d0424a55abc978892840c4",
+    "url": "/clicky-game/static/media/puppy9.96aab145.jpg"
+  }
+]);
